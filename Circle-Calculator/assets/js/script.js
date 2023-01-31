65deb2d195bc3e00fbfb8b1720950bbfe8a3437a
marshallmead077 <marshallmead.mm39@gmail.com>
2023-01-31 14:18:00 -0600

    update
    finished circle calculator and added  drop down menu for position lab

---
+++ Circle-Calculator/assets/js/script.js
@@ -0,0 +1,80 @@
+/* The following should be a file in your js or script folder */
+
+$( "#CircleForm" ).validate({
+
+});
+
+function displayCircle() {
+    // if the form is valid, then make the calculations
+    if ($("#CircleForm").valid()) {
+        
+         document.getElementById("diameter").innerHTML = "";
+
+         var radius; // string representation of the radius
+         var radius1fp; // floating point value of radius
+         
+         var diameter;  // floating point diameter
+         var circumference;  // floating point circumference
+         var area;  // floating point area
+         var result; // displayable result
+
+         // read in the legs as a string
+         radius = document.getElementById("radius").value;
+         circumference = document.getElementById("circumference").value;
+         area = document.getElementById("area").value;
+
+         // Convert numbers from strings to Floating Point
+         radiusfp = parseFloat( radius ); 
+         
+
+         // calculate the diameter
+         diameter = calcDiameter(radius);
+
+          // calculate the circumference
+          circumference = calcCircumference(radius);
+
+           // calculate the area
+         area = calcArea(radius);
+
+
+         // display the diameter
+         document.getElementById("diameter").innerHTML = diameter.toString();
+
+         // display the circumference
+         document.getElementById("circumference").innerHTML = circumference.toString();
+
+         // display the diameter
+         document.getElementById("area").innerHTML = area.toString();
+         
+    }
+}
+
+  function calcDiameter (radius)
+  // returns the diameter
+  // 2 times the radius
+  {
+      return 2 * radius;
+  }
+
+  function calcCircumference (radius)
+  // returns the circumeference 
+  // 2 times the radius
+  {
+      return 2 * Math.PI * radius;
+  }
+
+  function calcArea (radius)
+  // returns the area
+  // 2 times the radius
+  {
+      return Math.PI * radius * radius;
+  }
+  
+  function clearForm()
+{
+    document.getElementById("radius").value = "";
+    document.getElementById("radiuserror").innerHTML = "";
+    document.getElementById("circumference").value = "";
+    document.getElementById("circumferenceerror").innerHTML = "";
+    document.getElementById("hypotenuse").innerHTML = "";
+}
